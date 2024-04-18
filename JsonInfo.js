5b2e6763a670ba2a95d853e7bca2a37c5cddf3c1
aghil <aghil1373@gmail.com>
2024-04-18 18:22:45 +0330

    add json file

---
+++ JsonInfo.js
@@ -0,0 +1,3 @@
+var musicInfoJson = "{\"Information\":[{\"ID\":1,\"Title\":\"Title\",\"Singer\":\"Singer\",\"Image\":\"Image\",\"Text\":\"Text\"},{\"ID\":2,\"Title\":\"Title2\",\"Singer\":\"Singer2\",\"Image\":\"Image2\",\"Text\":\"Text2\"}]}";
+
+var musicInfo = JSON.parse(musicInfoJson);
