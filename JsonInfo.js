1aab2c7486b7fb5583e33acbdd08bf3ba9f7eb32
aghil <aghil1373@gmail.com>
2024-04-18 19:57:39 +0330

    use each in jquery over json info

--- JsonInfo.js
+++ JsonInfo.js
@@ -1,3 +1,10 @@
 var musicInfoJson = "{\"Information\":[{\"ID\":1,\"Title\":\"Title\",\"Singer\":\"Singer\",\"Image\":\"Image\",\"Text\":\"Text\"},{\"ID\":2,\"Title\":\"Title2\",\"Singer\":\"Singer2\",\"Image\":\"Image2\",\"Text\":\"Text2\"}]}";
 
 var musicInfo = JSON.parse(musicInfoJson);
+
+$.each(musicInfo.Information,function(index,value)
+{
+	console.log("index : " + index + " (title,singer,image,text): " + value.Title + "," , value.Singer + "," + value.Image +"," + value.Text + "," );
+});
+
+
